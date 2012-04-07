855c4d6a7afe06461aa2b1508a69667ce0398212
Tyler Perkins <tyler_perkins@me.com>
2012-04-07 16:35:51 -0600

    The Network Activity Indicator is now shown when there is a network connection, and it is hidden when all connections are closed and received data has been loaded into managed objects.

--- SmartTrail/BMAController.m
+++ SmartTrail/BMAController.m
@@ -14,13 +14,22 @@
 #import "EventWebClient.h"
 
 
+@interface BMAController ()
+- (void) aNetActivityDidStart;
+- (void) aNetActivityDidStop;
+@end
+
+
 @implementation BMAController
+{
+    NSInteger netActivitiesCount;
+}
 
 
 - (void) downloadAllTrailInfo {
 
     dispatch_queue_t q = dispatch_get_global_queue(
-        DISPATCH_QUEUE_PRIORITY_HIGH, 0
+        DISPATCH_QUEUE_PRIORITY_LOW, 0
     );
     dispatch_async( q, ^{
         CoreDataUtils* utils = [[CoreDataUtils alloc]
@@ -37,6 +46,8 @@
         ConditionWebClient* condClient = [[ConditionWebClient alloc]
             initWithDataUtils:utils regionId:1
         ];
+
+        [self aNetActivityDidStart];
 
         [areaClient sendSynchronousGet];
         if ( ! areaClient.error ) {
@@ -67,19 +78,20 @@
             }
         }
 
+        [self aNetActivityDidStop];
         [utils save];
 
-        [utils release];
         [condClient release];
         [trailClient release];
         [areaClient release];
+        [utils release];
     });
 }
 
 
 - (void) downloadEvents {
     dispatch_queue_t q = dispatch_get_global_queue(
-        DISPATCH_QUEUE_PRIORITY_HIGH, 0
+        DISPATCH_QUEUE_PRIORITY_LOW, 0
     );
     dispatch_async( q, ^{
         CoreDataUtils* utils = [[CoreDataUtils alloc]
@@ -90,11 +102,46 @@
         EventWebClient* eventClient = [[EventWebClient alloc]
             initWithDataUtils:utils regionId:1
         ];
+        [self aNetActivityDidStart];
         [eventClient sendSynchronousGet];
+        [self aNetActivityDidStop];
         [utils save];
 
         [eventClient release];
         [utils release];
+    });
+}
+
+
+#pragma mark - Private methods and functions
+
+
+/** Sent to indicate that a network activity began. Tracks the number of such
+    activities and displays the Network Activity Indicator if any activities
+    have started and not ended yet. A call to this method must be followed
+    at some point by a call to aNetActivityDidStop when the activity ends.
+*/
+- (void) aNetActivityDidStart {
+    dispatch_async( dispatch_get_main_queue(), ^{
+        netActivitiesCount++;
+        if ( netActivitiesCount > 0 ) {
+            [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+        }
+    });
+}
+
+
+/** Sent to indicate that a network activity ended. Tracks the number of such
+    activities and hides the Network Activity Indicator if all activities
+    have ended. A call to this method must eventually follow the call to
+    aNetActivityDidStart made when the activity began.
+*/
+- (void) aNetActivityDidStop {
+    dispatch_async( dispatch_get_main_queue(), ^{
+        netActivitiesCount--;
+        if ( netActivitiesCount <= 0 ) {
+            [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+        }
     });
 }
 
